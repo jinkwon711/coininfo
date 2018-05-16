7df055ff476db24dde96761077f6a1331607f30b
jin.kwon <jin.kwon@navercorp.com>
2018-05-16 19:14:34 +0900

    save as csv file

--- main.py
+++ main.py
@@ -1,43 +1,26 @@
 # 3rd party
 import pickle
 # my module
-from utils import get_coinlist,add_site_info,add_github_info
-
-
-
+from utils import get_coinlist, add_site_info, add_github_info
 
 
 coin_list = get_coinlist("BTC")[:5]
-
-new_coin_list = []
-for coin in coin_list:
-	new_coin_list.append(add_site_info(coin))
-
-for coin in new_coin_list:
-    add_github_info(coin)
-
-for coin in new_coin_list:
-    print(coin)
-
-# new_coin_list = []
-# for coin in coin_list:
-# 	new_coin_list.append(add_site_info(coin))
+attr = ["name", "code", "market", "active", "github", "website", "explorer1", "explorer2", "forum",
+        "announcement", "commits", "branchs", "releases", "contributors", "opened_issues", "closed_issues"]
+with open('./coinlist.csv', 'w') as f:
+    f.write(",".join(attr) + "\n")
+    new_coin_list = []
+    for coin in coin_list:
+        add_site_info(coin)
+        if coin.active == "True":
+            add_github_info(coin)
+            f.write(str(coin) + "\n")
 
 
-#save coin data as binary
+# save coin data as binary
 # with open("./coinlist.pickle","wb") as save_f:
 # 	pickle.dump(new_coin_list,save_f,protocol=pickle.HIGHEST_PROTOCOL)
 
 # load coin data
 # with open('./coinlist.pickle','rb') as load_f:
 # 	data = pickle.load(load_f)
-
-
-
-
-
-
-
-
-
-
--- utils.py
+++ utils.py
@@ -3,88 +3,92 @@ from time import sleep
 from classes.coin import Coin
 import requests
 
+
 def get_html(url):
-	response = requests.get(url)
-	return response.text
+    response = requests.get(url)
+    return response.text
+
 
 def get_coinlist(market="ALL"):
-	if market not in ["BTC","ETH","DOGE","ETC","LTC","ALL"]:
-		print(f"{market} market does not exist")
-	coinlist= []
-	response = requests.get("https://www.coinexchange.io/api/v1/getmarkets")
-	json_data = response.json()
-	try:
-		if json_data["message"]=="":
-			for item in json_data["result"]:
-				if item["BaseCurrencyCode"] in [market,"ALL"]:
-					coin = Coin(
-						item["MarketAssetName"],
-						item["MarketAssetCode"],
-						item["BaseCurrencyCode"],
-						str(item["Active"])
-						)
-					coinlist.append(coin)
-	except:
-		print("error occured",e)
-	return coinlist
+    if market not in ["BTC", "ETH", "DOGE", "ETC", "LTC", "ALL"]:
+        print(f"{market} market does not exist")
+    coinlist = []
+    response = requests.get("https://www.coinexchange.io/api/v1/getmarkets")
+    json_data = response.json()
+    try:
+        if json_data["message"] == "":
+            for item in json_data["result"]:
+                if item["BaseCurrencyCode"] in [market, "ALL"]:
+                    coin = Coin(
+                        item["MarketAssetName"],
+                        item["MarketAssetCode"],
+                        item["BaseCurrencyCode"],
+                        str(item["Active"])
+                    )
+                    coinlist.append(coin)
+    except:
+        print("error occured", e)
+    return coinlist
+
 
 def add_github_info(coin):
-	if coin.github == "":
-	#no github info just return
-		return
-	html = requests.get(coin.github).text
-	body = BeautifulSoup(html,"lxml").body
-	numbers =[x.text.replace("\n","").strip() for x in body.find_all("span",{"class":"num text-emphasized"})]
-	coin.set_github_info(*numbers)
+    if coin.github == "":
+        # no github info just return
+        return
+    html = requests.get(coin.github).text
+    body = BeautifulSoup(html, "lxml").body
+    numbers = [x.text.replace("\n", "").replace(",", "").strip(
+    ) for x in body.find_all("span", {"class": "num text-emphasized"})]
+    coin.set_github_info(*numbers)
 
-	issue_url = coin.github+"/issues"
-	html = requests.get(issue_url).text
-	issue_body = BeautifulSoup(html,"lxml").body
-	try:
-		opened_and_closed_issues = [
-			x.text.replace("\n","").split(" ")[6] 
-			for x in issue_body
-				.find("div",{"role":"main"})
-				.find("div",{"class":"table-list-header"})
-				.find_all("a")[:2]
-		]
-	except:
-		opened_and_closed_issues = ["0","0"]
-	coin.set_github_issue_info(*opened_and_closed_issues)
-	return
+    issue_url = coin.github + "/issues"
+    html = requests.get(issue_url).text
+    issue_body = BeautifulSoup(html, "lxml").body
+    try:
+        opened_and_closed_issues = [
+            x.text.replace("\n", "").replace(",", "").split(" ")[6]
+            for x in issue_body
+            .find("div", {"role": "main"})
+            .find("div", {"class": "table-list-header"})
+            .find_all("a")[:2]
+        ]
+    except:
+        opened_and_closed_issues = ["0", "0"]
+    coin.set_github_issue_info(*opened_and_closed_issues)
+    return
+
 
 def add_site_info(coin):
-	if(coin.code=="BTC"):
-		print(coin.code)
-		return coin
-	code = coin.code
-	market = coin.market
-	url = f"https://www.coinexchange.io/market/{code}/{market}"
-	print(coin.name)
-	print(url)
-	html = requests.get(url).text
-	soup = BeautifulSoup(html,"lxml")
-	coinlinks = soup.find("div",{"class":"coinlinks"})
-	if(coinlinks==None):
-		return coin
-	link_trs = coinlinks.table.find_all('tr')
+    if(coin.code == "BTC"):
+        print(coin.code)
+        return coin
+    code = coin.code
+    market = coin.market
+    url = f"https://www.coinexchange.io/market/{code}/{market}"
+    print(coin.name)
+    print(url)
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "lxml")
+    coinlinks = soup.find("div", {"class": "coinlinks"})
+    if(coinlinks == None):
+        return coin
+    link_trs = coinlinks.table.find_all('tr')
 
+    for link_tr in link_trs:
+        tds = link_tr.find_all("td")
 
-	for link_tr in link_trs:
-		tds = link_tr.find_all("td")
-
-		site_name = tds[1].text.strip()
-		site_link = tds[2].find("a",href=True).text
-		if site_name == "Announcement":
-			coin.set_announcement(site_link)
-		elif site_name == "Github":
-			coin.set_github(site_link)
-		elif site_name == "Explorer1":
-			coin.set_explore1(site_link)
-		elif site_name == "explorer2":
-			coin.set_explore2(site_link)
-		elif site_name == "Forum":
-			coin.set_forum(site_link)
-		elif site_name == "Website":
-			coin.set_website(site_link)
-	return coin
+        site_name = tds[1].text.strip()
+        site_link = tds[2].find("a", href=True).text
+        if site_name == "Announcement":
+            coin.set_announcement(site_link)
+        elif site_name == "Github":
+            coin.set_github(site_link)
+        elif site_name == "Explorer1":
+            coin.set_explore1(site_link)
+        elif site_name == "explorer2":
+            coin.set_explore2(site_link)
+        elif site_name == "Forum":
+            coin.set_forum(site_link)
+        elif site_name == "Website":
+            coin.set_website(site_link)
+    return coin
